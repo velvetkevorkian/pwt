69d5428387c38da561d571f49847afc6ac75c947
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2013-10-15 02:05:41 +0100

    dj billing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
-    $('.graphic h4, .date h2, .content h3').fitText();
+    $('.graphic h4, .date h2, .content h3, .djs h2').fitText();
+    $('.djs h5').fitText(1.5);
     
 });
