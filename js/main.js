7ec038be1a8fded3d89162adeea059b239384259
Kyle Macquarrie <kyle@velvetkevorkian.co.uk>
2013-10-15 15:15:50 +0100

    implement countdown

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $('.graphic h4, .date h2, .content h3, .djs h2').fitText();
     $('.djs h5').fitText(1.5);
     
+    var time= new Date(2013, 11-1, 7, 21, 0, 0);
+    
+    $('#countdown').countdown({
+        until: time 
+    });
+    
 });
